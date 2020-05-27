b59a92d3ca56a1abdd21d89c1d826bf20e3d5708
François Degrave <f.degrave@oncodna.com>
2020-05-27 11:47:51 +0200

    [IMP] better error messages when no rates provided

--- easypost_base/models/product.py
+++ easypost_base/models/product.py
@@ -6,16 +6,17 @@
 #
 ##############################################################################
 
-from odoo import models, fields, api, _
 import os
 from ast import literal_eval
-from odoo.addons.easypost_base.models.carrier import EP_PREFIX, set_field_ep_carriers_selection
+
+from odoo import models, fields, api
+from odoo.addons.easypost_base.models.carrier import set_field_ep_carriers_selection
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    description_delivery = fields.Text('Description for Delivery', help="Used for customs info")
+    description_delivery = fields.Text("Description for Delivery", help="Used for customs info")
 
 
 class ProductPackaging(models.Model):
@@ -24,8 +25,13 @@ class ProductPackaging(models.Model):
     @api.model
     def _setup_base(self, partial):
         super(ProductPackaging, self)._setup_base(partial)
-        if self.env['easypost_base.carrier']._fields:
-            set_field_ep_carriers_selection(self.env, self._fields['package_carrier_type'])
+        if self.env["easypost_base.carrier"]._fields:
+            set_field_ep_carriers_selection(self.env, self._fields["package_carrier_type"])
+
+    @api.model
+    def _setup_complete(self):
+        super(ProductPackaging, self)._setup_complete()
+        self.refresh_packaging()
 
     @api.model
     def refresh_packaging(self, delivery_type=None):
@@ -33,12 +39,16 @@ class ProductPackaging(models.Model):
         with open(os.path.join(os.path.dirname(__file__), "../data/packages.py")) as packages_file:
             packages_dic = literal_eval(packages_file.read())
             for carrier in packages_dic:
-                ep_carrier = self.env['easypost_base.carrier'].create({'name': carrier})
+                ep_carrier = self.env["easypost_base.carrier"].create({"name": carrier})
                 if delivery_type and ep_carrier.code != delivery_type:
                     continue
                 for pack_name in packages_dic[carrier]:
-                    exists = self.search([('package_carrier_type', '=', ep_carrier.code),
-                                          ('name', '=', pack_name)])
+                    exists = self.search([("package_carrier_type", "=", ep_carrier.code), ("name", "=", pack_name)])
                     if not exists and ep_carrier.installed:
-                        self.sudo().create({'name': pack_name, 'shipper_package_code': pack_name,
-                                            'package_carrier_type': ep_carrier.code})
+                        self.sudo().create(
+                            {
+                                "name": pack_name,
+                                "shipper_package_code": pack_name,
+                                "package_carrier_type": ep_carrier.code,
+                            }
+                        )
